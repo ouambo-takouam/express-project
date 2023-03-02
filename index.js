95d270b61b9f20c72a9b45c14f70af0d42763131
patrick ouambo <patrickouambo@gmail.com>
2023-03-02 10:05:53 +0100

    introducing express

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello world');
+	res.send({
+		id: 1,
+		name: 'Issac Newton',
+	});
+});
+
+app.get('/messages', (req, res) => {
+	res.send('<ul><li>This is your message</li></ul>');
+});
+
+app.post('/messages', (req, res) => {
+	console.log('updating messages ...');
 });
 
 app.listen(port, () => {
